cd9528d0aed21791f7bde541b8f2470321a00663
Henrii <151474844+eohenri@users.noreply.github.com>
2024-05-15 21:46:14 -0300

    Update votar.js

--- commands/app/votar.js
+++ commands/app/votar.js
@@ -14,7 +14,7 @@ module.exports = {
         const userdb = await User.findOne({ _id: message.author.id });
         if (!userdb) return message.reply({ content: `${message.author}, Você não coletou o daily hoje!` });
         if (Date.now() < userdb.cooldowns.topgg) return message.reply({ content: `<:nysa_cooldown:1217515334924505201> **|** Volte <t:${~~(userdb.cooldowns.topgg / 1000)}:R> para votar em mim novamente!` });
-        const api = new Topgg.Api("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExMDg1NjI2NzM4OTkxNDMxNjgiLCJib3QiOnRydWUsImlhdCI6MTcwMjY0MjgyOX0.paRX3TbqSzocwrG6AJBxNpqoYfvQkKcDj2dZlNiEwNE");
+        const api = new Topgg.Api("YOU_TOKEN_TOPGG");
         let votou = await api.hasVoted(`${message.author.id}`).catch(() => null);
         if (!votou) return message.reply({ content: `<:presente:1218187435620434010> ${message.author}, Você não votou em mim hoje!\nVotando em mim lhe darei uma recompensa de **10.000 gemas**!\n\nhttps://top.gg/bot/1108562673899143168/vote` });
         message.reply({ embeds: [ new EmbedBuilder().setTitle("✅ Votar").setColor(colors.white).setThumbnail("https://cdn-icons-png.flaticon.com/512/4213/4213958.png").setDescription(`Você **votou em mim** e como recompensa ganhou **<:gemas:1226215346743279616> 10,000 gemas**.`).setFooter({ text: `Utilize ${p}tempo para mostrar seus intervalos!` }) ] });
